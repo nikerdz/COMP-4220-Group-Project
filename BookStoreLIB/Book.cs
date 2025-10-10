64034331a4b0a10d4d19a38579157cf233b6a788
diajm <hinadiam@gmail.com>
2025-10-10 18:31:23 -0400

    Add/Remove Logic (initial)

---
+++ BookStoreLIB/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public class Book // for a single book
+    {
+        public string BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Quantity { get; set; } = 1;
+        public decimal Price { get; set; }
+        public int Year { get; set; }
+        public decimal Subtotal => Price * Quantity;
+
+    }
+}
---
+++ BookStoreLIB/Cart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public class Cart // all cart related actions
+    {
+        public List<Book> shoppingCart = new List<Book>(); // list of books for each customer cart
+        public bool addBook(Book book) // based on obj from DB booklist
+        {
+            if (book == null) return false;
+            shoppingCart.Add(book); // adds to shopping cart list
+            return true;
+        }
+        public bool removeBook(Book book) // removing cart books
+        {
+            if (shoppingCart.Contains(book))
+            {
+                shoppingCart.Remove(book);
+                return true;
+            }
+
+            return false;
+
+        }
+        public void clearCart() // clearing cart
+        {
+                shoppingCart.Clear();
+           
+        }
+    }
+}
