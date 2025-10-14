c7b1018fd5bea1c655fc6b4008a2a39005a35428
Anika Khan <anika.khan323@gmail.com>
2025-10-13 23:10:00 -0400

    - Created Cart Table in Database - Added Scripts sub-directory in Database directory - Added 20251013_AddCartTable.sql script to allow rest of teams to udpate their database instances - Added BookStoreDB.mdf to git.ignore to avoid pushing local database changes

---
+++ BookStoreGUI/Database/Scripts/20251013_AddCartTable.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (
+    SELECT * FROM sysobjects 
+    WHERE name = 'Cart' AND xtype = 'U'
+)
+BEGIN
+    CREATE TABLE [dbo].[Cart] (
+        [CartID] INT IDENTITY(1,1) PRIMARY KEY,
+        [CustomerID] INT NOT NULL,
+        [ISBN] CHAR(10) NOT NULL,
+        [Quantity] INT DEFAULT 1,
+        [Subtotal] DECIMAL(10,2),
+        CONSTRAINT [FK_Cart_Customer] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customer]([CustomerID]),
+        CONSTRAINT [FK_Cart_Book] FOREIGN KEY ([ISBN]) REFERENCES [dbo].[BookData]([ISBN])
+    );
+END;
