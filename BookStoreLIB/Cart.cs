cc391db2b59183136a24780f944994ef265801ec
diajm <hinadiam@gmail.com>
2025-10-17 18:42:47 -0400

    Quantities/duplicates, clear cart erorr handling check, XAML grid extra default row removed, additional hard coded rows, BookID fixed (string to int), removed buttons enabling/disabling

--- BookStoreLIB/Cart.cs
+++ BookStoreLIB/Cart.cs
@@ -12,16 +12,37 @@ namespace BookStoreLIB
         public List<Book> shoppingCart = new List<Book>(); // list of books for each customer cart
         public bool addBook(Book book) // based on obj from DB booklist
         {
-            if (book == null) return false;
-            shoppingCart.Add(book); // adds to shopping cart list
-            return true;
+            for (int i = 0; i < shoppingCart.Count; i++)
+            {
+                if (shoppingCart[i].BookID == book.BookID)
+                {
+                    shoppingCart[i].Quantity += 1;
+                    return true;
+                }
+            }
+
+                shoppingCart.Add(book);
+                return true;
         }
         public bool removeBook(Book book) // removing cart books
         {
-            if (shoppingCart.Contains(book))
+           
+            for (int i = 0; i < shoppingCart.Count; i++)
             {
-                shoppingCart.Remove(book);
-                return true;
+                if (shoppingCart[i].BookID == book.BookID)
+                {
+                    if (shoppingCart[i].Quantity > 1)
+                    {
+                        shoppingCart[i].Quantity -= 1;
+                    }
+                    else
+                    {
+                        shoppingCart.RemoveAt(i);
+                        
+                    }
+                    return true;
+                }    
+                
             }
 
             return false;
