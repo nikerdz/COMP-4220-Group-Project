01028a53d0b16a18c8da3f72af3f58a756fdd8f7
Aryan0744 <114934820+Aryan0744@users.noreply.github.com>
2025-10-27 23:03:20 -0400

    fix: add AdminDashboard event handlers and restore build success

---
+++ BookStoreGUI/AdminDashboard.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookStoreGUI
+{
+    public partial class AdminDashboard : Window
+    {
+        public AdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        // Header: Logout
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Logout clicked (TODO: implement sign-out)");
+            Close();
+        }
+
+        // Left Nav
+        private void NavInventory_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: load Inventory view into ContentHost
+            MessageBox.Show("Inventory clicked (TODO)");
+        }
+
+        private void NavCategories_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Categories clicked (TODO)");
+        }
+
+        private void NavOffers_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Offers clicked (TODO)");
+        }
+
+        private void NavOrders_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Orders clicked (TODO)");
+        }
+    }
+}
