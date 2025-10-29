b11639d30277d08663c2e341de35417dbd6f4122
raaadi <raad.islam1>
2025-10-29 14:47:11 -0400

    Add/Remove Logic Test Cases

---
+++ BookStoreLIB.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BookStoreLIB.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookStoreLIB.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("47cf82d8-e81e-479b-9dad-1a30d5b972d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BookStoreLIB/CartTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookStoreLIB;
+using System.Collections.Generic;
+//test
+namespace BookStoreLIB.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void AddBook_ShouldIncreaseCartCount()
+        {
+            
+            var cart = new Cart();
+            var book = new Book { BookID = 1, Title = "Test Book", Author = "Tester", Price = 10 };
+
+          
+            cart.addBook(book);
+
+            Assert.AreEqual(1, cart.shoppingCart.Count);
+        }
+
+        [TestMethod]
+        public void RemoveBook_ShouldDecreaseCartCount()
+        {
+            var cart = new Cart();
+            var book = new Book { BookID = 1, Title = "Test Book", Author = "Tester", Price = 10 };
+            cart.addBook(book);
+
+            cart.removeBook(book);
+
+            Assert.AreEqual(0, cart.shoppingCart.Count);
+        }
+
+        [TestMethod]
+        public void ClearCart_ShouldEmptyTheCart()
+        {
+            
+            var cart = new Cart();
+            cart.addBook(new Book { BookID = 1, Title = "Book A" });
+            cart.addBook(new Book { BookID = 2, Title = "Book B" });
+
+          
+            cart.clearCart();
+            Assert.AreEqual(0, cart.shoppingCart.Count);
+        }
+    }
+}
