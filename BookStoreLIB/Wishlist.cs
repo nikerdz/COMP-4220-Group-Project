736cdff1154e13471d2988dc2ee98408803eeb60
Anika Khan <anika.khan323@gmail.com>
2025-11-13 23:00:09 -0500

    feat: Add failing Wishlist unit tests and class/DAL stubs; good to begin writing functional wishlist code

---
+++ BookStoreLIB/Wishlist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public class Wishlist
+    {
+        public List<Book> wishlistBooks = new List<Book>();
+
+        public bool AddToWishlist(Book book)
+        {
+            throw new NotImplementedException(); // <- ensures test will fail if executed
+        }
+
+        public bool RemoveFromWishlist(string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearWishlist()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
