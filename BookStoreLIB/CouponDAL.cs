f150dfeb21f4a8a3eb23270cbec3c3b1cd8baff3
Anika Khan <anika.khan323@gmail.com>
2025-11-14 22:32:38 -0500

    feat: Add failing coupon unit tests and class/DAL stubs

---
+++ BookStoreLIB/CouponDAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public static class CouponDAL
+    {
+        // -------------------- Connection resolution --------------------
+        private static string ResolveConn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Coupon LoadCoupon(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IncrementUsage(int couponId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
