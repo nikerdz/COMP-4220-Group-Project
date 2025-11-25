f90e808a393c610d916210646eb9d2fcc2d6f96d
Hadiyah Arif <arifh@uwindsor.ca>
2025-11-24 20:19:36 -0500

    uncommented

--- BookStoreReact/bookstorereact.client/src/pages/PaymentForm.tsx
+++ BookStoreReact/bookstorereact.client/src/pages/PaymentForm.tsx
@@ -1,6 +1,6 @@
-//import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-//function proceedToCheckout() {
-//    setIsCheckoutOpen(true);
+function proceedToCheckout() {
+    setIsCheckoutOpen(true);
 
-//}
+}
